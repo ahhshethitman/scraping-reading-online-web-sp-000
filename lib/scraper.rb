5d4553767855d8a21f68867b363f1a4f911579f2
Hunter Sides <hunter.s.1337@gmail.com>
2020-03-19 17:09:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,4 +5,5 @@ html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 
-doc.css(".headline-26OIBN").text
+
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
